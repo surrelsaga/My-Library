c5b05dc1f03a90df23e5a21456c849bfeea8693c
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-05-05 13:55:52 +0800

    Add Book objects constructor and store in an array

--- script.js
+++ script.js
@@ -2,22 +2,37 @@ const myLibrary = [];
 
 // YOUR CODE HERE
 // Book constructor, addBookToLibrary, removeBookFromLibrary, displayBooks...
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary(title, author, pages, readStatus) {
+  const oneBook = new Book(title, author, pages, readStatus);
+  myLibrary.push(oneBook);
+}
+
+addBookToLibrary('H', 'G', 250, true);
+addBookToLibrary('G','K', 69, false);
+console.log(myLibrary);
 
 // =====================
 // UI BOILERPLATE
 // =====================
 
-const newBookBtn = document.getElementById("new-book-btn");
-const dialog = document.getElementById("book-dialog");
-const cancelBtn = document.getElementById("cancel-btn");
-const bookForm = document.getElementById("book-form");
+// const newBookBtn = document.querySelector("new-book-btn");
+// const dialog = document.querySelector("book-dialog");
+// const cancelBtn = document.querySelector("cancel-btn");
+// const bookForm = document.querySelector("book-form");
 
-newBookBtn.addEventListener("click", () => dialog.showModal());
-cancelBtn.addEventListener("click", () => dialog.close());
+// newBookBtn.addEventListener("click", () => dialog.showModal());
+// cancelBtn.addEventListener("click", () => dialog.close());
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault(); // prevents page reload on form submit
+// bookForm.addEventListener("submit", (e) => {
+//   e.preventDefault(); // prevents page reload on form submit
 
-  // YOUR CODE HERE
-  // read form values, call your functions, re-render, close dialog
-});
+//   // YOUR CODE HERE
+//   // read form values, call your functions, re-render, close dialog
+// });
